ece8df870f014a5882c8c66b3fb06f3f449a8931
jee7788 <jee7788@naver.com>
2023-05-23 22:13:43 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Box from './Box';
 
 function App() {
   const [size, setSizes] = useState(100);
+  const [isDark, setIsDark] =useState(false)
 
-  const createBoxStyle=()=>{
+  const createBoxStyle= useCallback( ()=>{
+
     return {
       backgroundColor : 'pink',
       width: `${size}px`,
       height: `${size}px`,
     };
-  };
+  },[size]);
 
   return (
-    <div>
+    <div style={{background: isDark ? 'black' : 'white',
+    }}>
       <input type='number'
       value={size}
       onChange={(e)=>setSizes(e.target.value)} />
+      <button onClick={()=>setIsDark(!isDark)}>Change Theme</button>
       <Box className='pink-box' createBoxStyle={createBoxStyle} />
      
     </div>
